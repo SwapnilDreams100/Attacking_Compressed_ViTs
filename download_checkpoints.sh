ae9c23c78668cba6e47f168b6ffa7e88b16efa83
Swapnil Parekh <swapnilbp100@gmail.com>
2022-05-17 04:34:48 -0400

    Update download_checkpoints.sh

--- download_checkpoints.sh
+++ download_checkpoints.sh
@@ -1,9 +1,11 @@
-mkdir -p ./checkpoints
-
-wget https://storage.googleapis.com/vit_models/imagenet21k%2Bimagenet2012/ViT-B_16-224.npz -O ./checkpoints/ViT-B_16-224.npz
 wget https://dl.fbaipublicfiles.com/deit/deit_tiny_patch16_224-a1311bcf.pth -O ./checkpoints/deit_tiny_patch16_224.pth
+wget https://dl.fbaipublicfiles.com/deit/deit_small_patch16_224-cd65a155.pth -O ./checkpoints/deit_small_patch16_224.pth
+wget https://dl.fbaipublicfiles.com/deit/deit_base_patch16_224-b5f2ef4d.pth -O ./checkpoints/deit_base_patch16_224.pth
+
+wget https://dl.fbaipublicfiles.com/deit/deit_tiny_distilled_patch16_224-b40b3cf7.pth -O ./checkpoints/deit_tiny_distilled_patch16_224.pth
+wget https://dl.fbaipublicfiles.com/deit/deit_small_distilled_patch16_224-649709d9.pth -O ./checkpoints/deit_small_distilled_patch16_224.pth
+wget https://dl.fbaipublicfiles.com/deit/deit_base_distilled_patch16_224-df68dfff.pth -O ./checkpoints/deit_base_distilled_patch16_224.pth
+
 wget https://github.com/DominickZhang/MiniViT-model-zoo/releases/download/v1.0.0/mini_deit_tiny_patch16_224.pth -O ./checkpoints/mini_deit_tiny_patch16_224.pth
-# wget https://storage.googleapis.com/vit_models/imagenet21k%2Bimagenet2012/ViT-B_16-224.npz -O ./checkpoints/ViT-B_16-224.npz
-# wget https://storage.googleapis.com/vit_models/imagenet21k%2Bimagenet2012/ViT-L_16-224.npz -O ./checkpoints/ViT-L_16-224.npz
-# wget https://storage.googleapis.com/vit_models/augreg/B_16-i1k-300ep-lr_0.001-aug_medium2-wd_0.03-do_0.0-sd_0.0--imagenet2012-steps_20k-lr_0.01-res_224.npz -O ./checkpoints/ViT-augreg_B_16-i1k-300ep-lr_0.001-aug_medium2-wd_0.03-do_0.0-sd_0.0--imagenet2012-steps_20k-lr_0.01-res-224.npz
-# wget https://storage.googleapis.com/vit_models/augreg/L_16-i1k-300ep-lr_0.001-aug_medium2-wd_0.03-do_0.0-sd_0.0--imagenet2012-steps_20k-lr_0.01-res_224.npz -O ./checkpoints/ViT-augreg_L_16-i1k-300ep-lr_0.001-aug_medium2-wd_0.03-do_0.0-sd_0.0--imagenet2012-steps_20k-lr_0.01-res-224.npz
+wget https://github.com/DominickZhang/MiniViT-model-zoo/releases/download/v1.0.0/mini_deit_small_patch16_224.pth -O ./checkpoints/mini_deit_small_patch16_224.pth
+wget https://github.com/DominickZhang/MiniViT-model-zoo/releases/download/v1.0.0/mini_deit_base_patch16_224.pth -O ./checkpoints/mini_deit_base_patch16_224.pth
